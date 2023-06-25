2bfc79a977a53fc42b05fb32ec2beae632ef2d5f
eteng <etengert23@hotmail.com>
2023-06-25 23:42:27 +1000

    Add a program to generate a monthly income report using an enumerate

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -8,19 +8,17 @@ def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
-
     for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
+    income_and_month = enumerate(incomes, 1)
+    print_income_report(income_and_month)
 
-    print_income_report(incomes, number_of_months)
 
-
-def print_income_report(incomes, number_of_months):
+def print_income_report(income_and_month):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in income_and_month:
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
