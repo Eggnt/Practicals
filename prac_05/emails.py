af9d4f5fee1d36b17e3d5ae2ead620992ef7bcb9
eteng <etengert23@hotmail.com>
2023-06-29 13:19:08 +1000

    Complete prac 5 emails exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,7 +2,22 @@
 
 email_to_name = {}
 
+
 def main():
+    email_address = str(input("Email: "))
+    name = convert_email_address_to_name(email_address)
+    choice = str(input(f"Is your name {name}? (Y/n) "))
+    if choice.upper() != "Y" and choice.upper() != "":
+        name = str(input("Name: "))
+    email_to_name[email_address] = name
+    print(email_to_name)
+
+
+def convert_email_address_to_name(email_address):
+    potential_name = email_address.split("@")
+    potential_name.pop(-1)
+    name = ((" ".join(str(potential_name).split("."))).title()).strip("[]'")
+    return name
 
 
 main()
