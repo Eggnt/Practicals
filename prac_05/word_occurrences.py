9dd6aa5ee900683078276bd6b1d4a215fcc8b447
eteng <etengert23@hotmail.com>
2023-06-29 12:38:48 +1000

    Complete prac 5 word occurrences exercise

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,19 @@
+"""
+Word Occurrences
+Estimate: 25 minutes
+Actual:   19 minutes
+"""
+
+word_to_count = {}
+
+
+def main():
+    sentence = str(input("Please enter a sentence: "))
+    for word in sentence.split(" "):
+        word_to_count[word] = word_to_count.get(word, 0) + 1
+        max_length = max(len(word) for word in list(word_to_count.keys()))
+    for word, count in sorted(word_to_count.items()):
+        print(f"{word:{max_length}} : {count}")
+
+
+main()
