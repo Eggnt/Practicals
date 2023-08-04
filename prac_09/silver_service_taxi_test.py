4a1f57e84b297155eae42eddcdb97fcb023e4701
Eggnt <etengert23@hotmail.com>
2023-08-04 22:57:57 +1000

    Update silver_service_taxi_test.py to display correct rounding on fare

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,7 +4,7 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 def main():
     my_silver_service_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
     my_silver_service_taxi.drive(18)
-    print(f"{my_silver_service_taxi}\nCurrent Fare: {my_silver_service_taxi.get_fare()}")
+    print(f"{my_silver_service_taxi}\nCurrent Fare: {my_silver_service_taxi.get_fare():.2f}")
 
 
 main()
