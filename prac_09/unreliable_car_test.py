5bb4f69e22aab70d02420a0db0c7226aa931a0e6
Eggnt <etengert23@hotmail.com>
2023-08-04 22:33:11 +1000

    Test unreliable_car.py

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,10 @@
+from prac_09.unreliable_car import UnreliableCar
+
+
+def main():
+    my_unreliable_car = UnreliableCar("Datsun", 100, 45)
+    my_unreliable_car.drive(70)
+    print(my_unreliable_car)
+
+
+main()
